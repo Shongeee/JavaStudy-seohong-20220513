b8b85dba11344506fecaf27c17037a0f53c13911
Shongeee <hongeever@gmail.com>
2022-06-08 21:52:52 +0900

    220608 수업

--- JavaStudy_seohong/src/j22_람다식/Math.java
+++ JavaStudy_seohong/src/j22_람다식/Math.java
@@ -1,6 +1,6 @@
 package j22_람다식;
 
-@FunctionalInterface
+@FunctionalInterface //람다식 사용 시 작성 -> 람다식은 무조건 하나의 메소드만 있어야 함.
 public interface Math<T1, T2> {
 	public double calc(T1 value1, T2 value2);
 }
